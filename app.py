3dfcda4667612be9a37cc9797e90076a7e266771
Aleksandra Adamus <aleksandra.adamus1993@gmail.com>
2024-09-16 15:50:36 +0200

    clean up code comments and spacing

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def initialize_database():
 
 
 initialize_database()
+database = database_manager.create_chroma_database()
 
 
 @cl.on_chat_start
@@ -38,12 +39,9 @@ async def on_chat_start():
         Initialize the chat session, set session variables, and send a welcome message.
     """
     logger.info("Chat started. Initializing session variables.")
-    database = database_manager.create_chroma_database()
     history = chat_manager.get_session_history()
-
     cl.user_session.set("database", database)
     cl.user_session.set("history", history)
-
     logger.info("Sending welcome message to user.")
     await cl.Message(content="I am a virtual gardening assistant. "
                              "I will help you choose the perfect plant for your garden. "
@@ -57,18 +55,13 @@ async def main(message: cl.Message):
         Handle incoming messages from the user, generate a response, and check if the conversation has ended.
     """
     logger.info(f"Received message from user: {message.content}")
-
-    database = cl.user_session.get("database")
     history = cl.user_session.get("history")
-
     response = chat_manager.get_response(user_input=message.content)
     logger.info(f"Generated response: {response}")
-
     if conversation_ended(response):
         logger.info("Conversation ended detected. Handling conversation end.")
         await handle_conversation_end(history, database)
         await end()
-
     else:
         logger.info("Sending response to user.")
         await cl.Message(content=response).send()
@@ -116,14 +109,11 @@ async def handle_conversation_end(history: str, database: Chroma):
                                                                 history=history)
     logger.info(f"Extracted plant groups: {plant_groups}")
     rag_manager = RAGManager(prompt_manager=prompt_manager, vectorstore=database)
-
     retriever = rag_manager.create_retriever(plant_groups=plant_groups)
     retriever_docs = retriever.invoke(plant_description)
     logger.info(f"Retrieved documents: {[doc.page_content for doc in retriever_docs]}")
-
     response = rag_manager.get_response(plant_groups=plant_groups, plant_description=plant_description)
     logger.info(f"Response: {response}")
-
     dict_of_plants = extractor.extract_plant_names_and_image_paths(prompt_manager=prompt_manager,
                                                                    prompt_file_name="extract_dict", history=response)
     logger.info(f"Extracted plants from response: {dict_of_plants}")
@@ -134,11 +124,11 @@ async def handle_conversation_end(history: str, database: Chroma):
     logger.info(f"Final response generated: {final_response}")
     chat_manager.clear_session_history()
     logger.info("Cleared session history.")
-
     await cl.Message(content=final_response).send()
     for plant_name, image_path in dict_of_plants.items():
-        image = cl.Image(path=f"data/images/{image_path}", display='inline', size='large', name='plant')
-        await cl.Message(
-            content=plant_name,
-            elements=[image],
-        ).send()
+        if image_path != "9a58e99369a6799c9fc054c69935d7a509541b9e.jpg":
+            image = cl.Image(path=f"data/images/{image_path}", display='inline', size='large', name='plant')
+            await cl.Message(
+                content=plant_name,
+                elements=[image],
+            ).send()
